4713070da3a018e8553d9b3521923e700d014d6d
roselyn <roselynh100@gmail.com>
2025-11-17 13:57:24 -0500

    tap outside to unfocus keyboard

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
-import { ActivityIndicator, Button, Modal, View } from "react-native";
+import {
+  ActivityIndicator,
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  Modal,
+  ScrollView,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import { useRouter } from "expo-router";
 
 import { ThemedText } from "@/components/themed-text";
@@ -25,38 +34,46 @@ export default function HomeScreen() {
   };
 
   return (
-    <View className="flex flex-col gap-4 p-12">
-      <Modal visible={isLoading} transparent={true} animationType="fade">
-        <View className="flex-1 justify-center items-center gap-8 bg-black/70">
-          <ActivityIndicator size="large" />
-          <ThemedText type="subtitle">Saving your trip...</ThemedText>
-        </View>
-      </Modal>
+    <KeyboardAvoidingView>
+      <ScrollView keyboardShouldPersistTaps="handled">
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View className="flex flex-col gap-4 p-12">
+            <Modal visible={isLoading} transparent={true} animationType="fade">
+              <View className="flex-1 justify-center items-center gap-8 bg-black/70">
+                <ActivityIndicator size="large" />
+                <ThemedText type="subtitle">Saving your trip...</ThemedText>
+              </View>
+            </Modal>
 
-      <ThemedText type="title">Input your trip details</ThemedText>
+            <ThemedText type="title">Input your trip details</ThemedText>
 
-      <ThemedText type="subtitle">Destination</ThemedText>
-      <ThemedTextInput
-        value={destination}
-        onChangeText={onChangeDestination}
-        placeholder="Toronto, Canada"
-      />
+            <ThemedText type="subtitle">Destination</ThemedText>
+            <ThemedTextInput
+              value={destination}
+              onChangeText={onChangeDestination}
+              placeholder="Toronto, Canada"
+            />
 
-      <ThemedText type="subtitle">Trip Dates</ThemedText>
-      <ThemedTextInput
-        value={dates}
-        onChangeText={onChangeDates}
-        placeholder="May 1, 2026 - May 31, 2026"
-      />
+            <ThemedText type="subtitle">Trip Dates</ThemedText>
+            <ThemedTextInput
+              value={dates}
+              onChangeText={onChangeDates}
+              placeholder="May 1, 2026 - May 31, 2026"
+            />
 
-      <ThemedText type="subtitle">Activities Planned (Optional)</ThemedText>
-      <ThemedTextInput
-        value={activities}
-        onChangeText={onChangeActivities}
-        placeholder="Hiking, Fancy Dinner, Clubbing..."
-      />
+            <ThemedText type="subtitle">
+              Activities Planned (Optional)
+            </ThemedText>
+            <ThemedTextInput
+              value={activities}
+              onChangeText={onChangeActivities}
+              placeholder="Hiking, Fancy Dinner, Clubbing..."
+            />
 
-      <Button title="Save" onPress={handleSave} />
-    </View>
+            <Button title="Save" onPress={handleSave} />
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
