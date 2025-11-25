d5bf58f6bee8c02b02221ff34c7411de246108ef
ksacbibi <34385626+ksacbibi@users.noreply.github.com>
2025-11-24 21:03:44 -0500

    Edit algorithm to match models.py

--- backend/machine_learning/poc_decision_model.py
+++ backend/machine_learning/poc_decision_model.py
@@ -16,22 +16,23 @@ def get_item_importance(item: Item, trip: Trip) -> int:
     if item.cv_results and len(item.cv_results) > 0:
         name = item.cv_results[0].item_name
 
+    score = 0
     if name in ["Laptop", "Laptop Charger"]:
-        item.importance = 80
+        score = 80
     elif name in ["Toothbrush", "Toothpaste"]:
-        item.importance = 90
+        score = 90
     elif name in ["Shirt", "Pants", "Socks", "Shoes"]:
-        item.importance = 95
+        score = 95
     elif name == "Coat":
-        item.importance = 70
+        score = 70
     elif name == "Umbrella":
-        item.importance = 35
+        score = 35
     elif name == "Sunglasses":
-        item.importance = 30
+        score = 30
     elif name == "Snack":
-        item.importance = 20
+        score = 20
     else:
-        item.importance = 0
+        score = 0
 
     # Rules
 
@@ -39,36 +40,52 @@ def get_item_importance(item: Item, trip: Trip) -> int:
         name in ["Laptop", "Laptop Charger"]
         and "work" not in (trip.activities or "").lower()
     ):
-        item.importance = 0
+        score = 0
 
     # Not sure how weather is going to work yet
     # if name == "Coat" and "hot" in (trip.weather or "").lower():
-    #     item.importance = 0
+    #     score = 0
 
-    return item.importance
+    item.item_importance = score
+    return score
 
 
-def removal_recommendation_algorithm(item: Item, trip: Trip) -> RemovalRecommendation:
+def removal_recommendation_algorithm(
+    new_item: Item, trip: Trip, current_items: List[Item]
+) -> RemovalRecommendation:
     """Returns packing status i.e. whether items must be removed."""
+    # current_items would be something like:
+    # current_items_objects = []
+    #    for item_id in trip.items:
+    #        if item_id in items_store:
+    #            current_items_objects.append(items_store[item_id])
+    # removal_recommendation_algorithm(new_item=new_item, trip=trip, current_items=current_items_objects)
 
-    get_item_importance(item)
-    min_item_importance = min(get_item_importance(i) for i in trip.items)
+    # Calculate importance of new item
+    get_item_importance(new_item, trip)
+    # Find the minimum importance score of already packed items
+    if not current_items:
+        min_item_importance = 0
+    else:
+        for i in current_items:
+            get_item_importance(i, trip)
+        min_item_importance = min(i.item_importance for i in current_items)
 
     # Hard coded limits for now TODO: figure out where to put these
     WEIGHT_LIMIT_KG = 20.0
     VOLUME_LIMIT_CM3 = 50000.0
 
     # Check Weight
-    if trip.total_items_weight + item.weight_kg > WEIGHT_LIMIT_KG:
-        if item.importance > min_item_importance:
+    if trip.total_items_weight + new_item.weight_kg > WEIGHT_LIMIT_KG:
+        if new_item.item_importance > min_item_importance:
 
             # Order by importance DESC and add items to list until overflow is fixed
             weight_overflow = (
-                trip.total_items_weight + item.weight_kg
+                trip.total_items_weight + new_item.weight_kg
             ) - WEIGHT_LIMIT_KG
             candidates = []
             weight_cleared = 0.0
-            for i in sorted(trip.items, key=lambda x: x.importance):
+            for i in sorted(current_items, key=lambda x: x.item_importance):
                 candidates.append(i)
                 weight_cleared += i.weight_kg
 
@@ -89,16 +106,16 @@ def removal_recommendation_algorithm(item: Item, trip: Trip) -> RemovalRecommend
             )
 
     # Check Volume
-    if trip.total_items_volume + item.estimated_volume_cm3 > VOLUME_LIMIT_CM3:
-        if item.importance > min_item_importance:
+    if trip.total_items_volume + new_item.estimated_volume_cm3 > VOLUME_LIMIT_CM3:
+        if new_item.item_importance > min_item_importance:
 
             # Order by importance DESC and add items to list until overflow is fixed
             volume_overflow = (
-                trip.total_items_volume + item.estimated_volume_cm3
+                trip.total_items_volume + new_item.estimated_volume_cm3
             ) - VOLUME_LIMIT_CM3
             candidates = []
             volume_cleared = 0.0
-            for i in sorted(trip.items, key=lambda x: x.importance):
+            for i in sorted(current_items, key=lambda x: x.item_importance):
                 candidates.append(i)
                 volume_cleared += i.estimated_volume_cm3
 
@@ -119,8 +136,10 @@ def removal_recommendation_algorithm(item: Item, trip: Trip) -> RemovalRecommend
             )
 
     # Item is successfully packed:
-    trip.total_items_weight += item.weight_kg
-    trip.total_items_volume += item.estimated_volume_cm3
+    trip.total_items_weight += new_item.weight_kg
+    trip.total_items_volume += new_item.estimated_volume_cm3
+
+    trip.items.append(new_item.item_id)
 
     return RemovalRecommendation(
         status=RemovalRecommendationStatus.pack, reason=None, swap_candidates=None
